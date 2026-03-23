e9cfb46c0604df4e6dc381e7f3e3bdbf946d597a
Dilpreet <dilpreetmutreja33@gmail.com>
2026-03-23 06:37:47 +0000

    initial terraform setup

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "infra_demo" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "infra-demo"
+    Project     = "week7"
+    Owner       = "prashant"
+    Environment = "dev"
+  }
+}
